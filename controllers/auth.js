a7fbfa97fc58841a3a4303aa32e8a9e45b8ab19d
Pedro Bolig <pedro.bolig@gmail.com>
2021-05-11 15:06:44 -0300

    20210511 - Fin sección 10

---
+++ controllers/auth.js
@@ -0,0 +1,54 @@
+const { response } = require('express');
+const bcrypt = require('bcryptjs');
+
+const Usuario = require('../models/usuario');
+const { generarJWT } = require('../helpers/jwt');
+
+const login = async(req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+        // Verificar email
+        const usuarioDB = await Usuario.findOne({ email });
+
+        if (!usuarioDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'email no encontrado'
+            });
+        }
+
+        // Verificar constraseña
+        const validaPassword = bcrypt.compareSync(password, usuarioDB.password);
+        if (!validaPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Contraseña no válida'
+            });
+        }
+
+        // Generar el TOKEN - JWT
+        const token = await generarJWT(usuarioDB.id);
+
+        console.log(token);
+
+        res.json({
+            ok: true,
+            token
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Pongase en contacto con el administradorrrr'
+        })
+    }
+
+}
+
+module.exports = {
+    login
+}
---
+++ routes/usuarios.js
@@ -0,0 +1,40 @@
+/*
+    Ruta: /api/usuarios
+*/
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+
+const { getUauarios, crearUauarios, actualizarUsuario, borrarUsuario } = require('../controllers/usuarios');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const router = Router();
+
+router.get('/', validarJWT, getUauarios);
+
+router.post('/', [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('password', 'El password es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        validarCampos,
+    ],
+    crearUauarios
+);
+
+router.put('/:id', [
+        validarJWT,
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('role', 'El rol es obligatorio').not().isEmpty(),
+        validarCampos,
+    ],
+    actualizarUsuario
+);
+
+router.delete('/:id',
+    validarJWT,
+    borrarUsuario
+);
+
+module.exports = router;
